1420c379a10e9e447d05af9863fead6ecf831b82
osamtimizer <eaten.jb@gmail.com>
2019-05-18 19:28:19 +0900

    impl skeleton

--- rpn/src/main.rs
+++ rpn/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn rpn(exp: &str) -> f64 {
+    return 0.0
+}
+
+fn apply2<F>(stack: &mut Vec<f64>, fun: F)
+where
+    F: Fn(f64, f64) -> f64,
+    {
+    }
