30356755caded857b94f8bef60579fc07367a486
osamtimizer <eaten.jb@gmail.com>
2019-05-18 19:59:47 +0900

    imple apply2

--- rpn/src/main.rs
+++ rpn/src/main.rs
@@ -33,8 +33,18 @@ fn rpn(exp: &str) -> f64 {
     stack.pop().expect("Stack underflow")
 }
 
+// スタックから数値を2つ取り出し、F型のクロージャfunで計算し、結果をスタックに積む
+// ミュータブルなVec<f64>型のstackと、F型のfunを取るメソッド。Fはこの関数がジェネリクスであることを表す。
 fn apply2<F>(stack: &mut Vec<f64>, fun: F)
+//F型のトレイト境界
 where
     F: Fn(f64, f64) -> f64,
     {
+        // 変数yとxをスタックの最後の2要素に束縛する
+        if let (Some(y), Some(x)) = (stack.pop(), stack,pop()) {
+            let z = fun(x, y);
+            stack.push(z);
+        } else {
+            panic!("Stack underflow");
+        }
     }
